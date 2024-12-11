5b6d0a65e7f78fac47db3b07e98a2a7ec60c350a
Caleb Keene <calebkeene@icloud.com>
2024-12-10 19:33:52 -0500

    fix bug where file_ext.py would incorrectly report

--- file_ext.py
+++ file_ext.py
@@ -5,7 +5,7 @@ import re, sys
 log = sys.argv[1]
 dictionary = {}
 
-regex = re.compile(r'\.(7z|ai|apk|avi|bat|bin|bmp|c|cpp|csv|css|dat|deb|dll|dmg|doc|docx|exe|flv|gif|h|html|ico|iso|jar|java|jpeg|jpg|js|json|log|md|mov|mp3|mp4|pdf|php|png|ppt|pptx|psd|py|rar|rb|rpm|sh|sql|svg|tar|txt|wav|xls|xlsx|xml|yaml|zip)', re.IGNORECASE)
+regex = re.compile(r'\.(7z|ai|apk|avi|bat|bin|bmp|cpp|csv|css|c|dat|deb|dll|dmg|doc|docx|exe|flv|gif|html|h|ico|iso|jar|java|jpeg|jpg|json|js|log|md|mov|mp3|mp4|pdf|php|png|ppt|pptx|psd|py|rar|rb|rpm|sh|sql|svg|tar|txt|wav|xls|xlsx|xml|yaml|zip)', re.IGNORECASE)
 with open(log) as file:
     for line in file:
         search = re.search(regex, line)
